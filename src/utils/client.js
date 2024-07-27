2d83487500abe69e35b7e59108e3e39aacd43997
osas4real <aghahowaosamudiamen03@gmail.com>
2024-07-27 13:36:27 +0100

    moved server tokens to env file

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://zkcdrtkkehpvahragqat.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprY2RydGtrZWhwdmFocmFncWF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQwMTg2NTEsImV4cCI6MjAxOTU5NDY1MX0.IgZY-kgGEIoA0fw13WzHs5W_mzTUbSZ6hDGJffbSs5g"
+  import.meta.env.VITE_SUPABASE_PROJECT,
+  import.meta.env.VITE_SUPABASE_KEY
 );
-
 
 export default supabase;
