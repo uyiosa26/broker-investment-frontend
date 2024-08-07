2012994cb720d6f65fb863bfc92a0a851a96d864
osas4real <aghahowaosamudiamen03@gmail.com>
2024-08-07 12:47:18 +0100

    secured env variables

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://tlhcxgvltgfjujhybxpg.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsaGN4Z3ZsdGdmanVqaHlieHBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ5OTkzMTIsImV4cCI6MjAyMDU3NTMxMn0.QhNlIpTCBfYBfbUIbEIEwdz1ey3H4Is-KJLTCA1UGPc"
+  import.meta.env.VITE_SUPABASE_PROJECT,
+  import.meta.env.VITE_SUPABASE_KEY
 );
-
 
 export default supabase;
